4af9c9b226bb2ec06b062883cd403b8fc0c54c02
Aretha Fontaine <arethafontaine21@gmail.com>
2026-04-10 15:46:58 -0500

    Add localhost IP to CORS allowed origins

--- Back-end implementation/main.py
+++ Back-end implementation/main.py
@@ -28,7 +28,10 @@ app = FastAPI(title="MSU Surplus Tracker API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # your Next.js frontend
+    allow_origins=[
+        "http://localhost:3000",
+        "http://127.0.0.1:3000",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
